94209ff7bd5c48e45c636b57e23f230dc6a132d1
yhql <victor_servant@hotmail.com>
2023-03-14 17:08:02 +0100

    Do not generate api_level for Nano S

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,7 @@ use utils::*;
 
 #[derive(Debug, Deserialize)]
 struct NanosMetadata {
-    api_level: String,
+    api_level: Option<String>,
     curve: Vec<String>,
     path: Vec<String>,
     flags: String,
@@ -91,7 +91,7 @@ fn main() {
         } => (d, a, r),
     };
 
-    let device_str = <Device as Into<&str>>::into(device);
+    let device_str = <Device as Into<&str>>::into(device.clone());
     let device_json = format!("{}.json", &device_str);
     let device_json_path = Path::new(&ledger_target_path).join(&device_json);
     let exe_path = match cli.use_prebuilt {
@@ -190,7 +190,7 @@ fn main() {
 
     // create manifest
     let file = fs::File::create(&app_json).unwrap();
-    let json = json!({
+    let mut json = json!({
         "name": this_metadata.name.as_ref().unwrap_or(&this_pkg.name),
         "version": &this_pkg.version,
         "icon": icon,
@@ -200,10 +200,14 @@ fn main() {
             "curves": this_metadata.curve,
             "paths": this_metadata.path
         },
-        "apiLevel": this_metadata.api_level,
         "binary": hex_file,
         "dataSize": data_size
     });
+    // Ignore apiLevel for Nano S as it is unsupported for now
+    match device {
+        Device::Nanos => (),
+        _ => json["apiLevel"] = serde_json::Value::String(this_metadata.api_level.expect("Missing field"))
+    }
     serde_json::to_writer_pretty(file, &json).unwrap();
 
     if is_load {
